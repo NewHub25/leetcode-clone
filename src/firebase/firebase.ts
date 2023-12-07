65d9cdce18dd2c6fbc411c32fa9370cd5f617e04
Terry <master_correo144@hotmail.com>
2023-12-07 12:58:19 -0500

    Installation firebase

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDbFO8AqU-mQKy04GVZp7W1lVas9GXtbio",
+  authDomain: "leetclone-peru.firebaseapp.com",
+  projectId: "leetclone-peru",
+  storageBucket: "leetclone-peru.appspot.com",
+  messagingSenderId: "840203870076",
+  appId: "1:840203870076:web:a08d2156aa47f655ad0ef9"
+};
+
+const app = initializeApp(firebaseConfig);
