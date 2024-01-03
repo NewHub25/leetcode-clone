eca5c8d5ed565380a77014fdd295f0a7d201f023
Terry <master_correo144@hotmail.com>
2024-01-03 13:55:37 -0500

    ADD topbar to dynamic page

---
+++ src/pages/problems/[pid].tsx
@@ -0,0 +1,13 @@
+import Topbar from "@/components/topbar/topbar";
+import React from "react";
+
+type ProblemPageProps = {};
+
+const ProblemPage: React.FC<ProblemPageProps> = () => {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
+};
+export default ProblemPage;
