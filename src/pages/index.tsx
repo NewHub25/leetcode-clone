ea39d9011f82c2e95962ded1776257968869f7d5
Terry <master_correo144@hotmail.com>
2024-01-02 23:31:12 -0500

    SET topbar basic bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import Topbar from "@/components/topbar/topbar";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Hello world!</h1>
+    <main className="bg-dark-layer-2 min-h-screen">
+      <Topbar />
     </main>
   );
 }
