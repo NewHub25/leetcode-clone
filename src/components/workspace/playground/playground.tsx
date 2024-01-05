be3b883b2dad5958ab7dec98737638f55a9f820f
Terry <master_correo144@hotmail.com>
2024-01-05 10:51:27 -0500

    ADD boilerPlate const

--- src/components/workspace/playground/playground.tsx
+++ src/components/workspace/playground/playground.tsx
@@ -9,6 +9,10 @@ import EditorFooter from "./editor-footer/editor-footer";
 type PlaygroundProps = {};
 
 const Playground: React.FC<PlaygroundProps> = () => {
+  const boilerPlate = `function twoSum(nums, target) {
+    // Write your code here
+}`;
+  
   return (
     <div className="flex flex-col bg-dark-layer-1 relative overflow-x-hidden">
       <PreferenceNav />
@@ -20,7 +24,7 @@ const Playground: React.FC<PlaygroundProps> = () => {
       >
         <div className="w-full overflow-auto">
           <ReactCodeMirror
-            value="const a = 1"
+            value={boilerPlate}
             theme={vscodeDark}
             extensions={[javascript()]}
             style={{ fontSize: 16 }}
