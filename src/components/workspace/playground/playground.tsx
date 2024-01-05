76cc96a621a5ece7225669abdb8c999b218cf6da
Terry <master_correo144@hotmail.com>
2024-01-05 10:17:19 -0500

    ADD test cases and ADD styles from Tailwind

--- src/components/workspace/playground/playground.tsx
+++ src/components/workspace/playground/playground.tsx
@@ -26,12 +26,51 @@ const Playground: React.FC<PlaygroundProps> = () => {
           />
         </div>
         <div className="w-full px-5 overflow-auto">
+          {/* Testcase heading */}
           <div className="flex h-10 items-center space-x-6">
             <div className="relative flex h-full flex-col justify-center cursor-pointer">
               <div className="text-sm font-medium leading-5 text-white">
                 Test cases
               </div>
-              <hr className="absolute bottom-0 h-0.5 w-16 rounded-full border-none bg-white" />
+              <hr className="absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white" />
+            </div>
+          </div>
+
+          <div className="flex">
+            {/* case 1 */}
+            <div className="mr-2 items-start mt-2 text-white">
+              <div className="flex flex-wrap items-center gap-y-4">
+                <div className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap">
+                  Test 1
+                </div>
+              </div>
+            </div>
+            {/* case 2 */}
+            <div className="mr-2 items-start mt-2 text-white">
+              <div className="flex flex-wrap items-center gap-y-4">
+                <div className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap">
+                  Test 2
+                </div>
+              </div>
+            </div>
+            {/* case 3 */}
+            <div className="mr-2 items-start mt-2 text-white">
+              <div className="flex flex-wrap items-center gap-y-4">
+                <div className="font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap">
+                  Test 3
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="font-semibold my-4">
+            <p className="text-sm font-medium mt-4 text-white">Input: </p>
+            <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+              nums: [2,7,11,15], target: 9
+            </div>
+            <p className="text-sm font-medium mt-4 text-white">Output: </p>
+            <div className="w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2">
+              [0, 1]
             </div>
           </div>
         </div>
