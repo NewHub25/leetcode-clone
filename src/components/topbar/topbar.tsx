3fc59d7a1372c43f145f8600b4bab0f190c5509f
Terry <master_correo144@hotmail.com>
2024-01-11 00:01:04 -0500

    FIX bug of Timer

--- src/components/topbar/topbar.tsx
+++ src/components/topbar/topbar.tsx
@@ -70,7 +70,7 @@ const Topbar: React.FC<TopbarProps> = ({ problemPage }) => {
               </button>
             </Link>
           )}
-          {problemPage && <Timer />}
+          {user && problemPage && <Timer />}
           {user && (
             <div className="cursor-pointer group relative">
               <Image
